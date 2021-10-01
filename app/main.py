6cf9283323cc6dcf701122c79af97b49a47c149b
Fixxy <kewell5@live.com>
2021-10-01 20:49:12 +0700

    change environment

--- app/main.py
+++ app/main.py
@@ -8,13 +8,13 @@ app = Flask(__name__)
 auth = HTTPBasicAuth()
 
 
-
 @auth.verify_password
 def verify_password(username, password):
-    res = Authenticate(username,password)
+    res = Authenticate(username, password)
     if not res:
         return False
     return True
+
 
 @app.route("/")
 def index() -> str:
@@ -32,11 +32,8 @@ def SendNotify() -> str:
 @app.route("/sendstocknotify", methods=['POST'])
 @auth.login_required
 def SendStockNotify() -> str:
-    sendlist = ['or','ptt','tisco','scc','scgp','kbank','spvi']
+    sendlist = ['or', 'ptt', 'amanah', 'scc', 'scgp', 'kbank', 'spvi']
     msg = GetSetData(sendlist)
     print(msg)
     response = {"message": SendLineNotify(msg)}
     return jsonify(response)
-
-
-    
--- conftest.py
+++ conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from app import main
 
-# see: http://flask.pocoo.org/docs/1.0/testing/
+
 @pytest.fixture
 def client():
     main.app.config['TESTING'] = True
